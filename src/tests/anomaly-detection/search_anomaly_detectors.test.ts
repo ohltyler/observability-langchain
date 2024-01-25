a0f0735cc7cc8373967a66b81500159d82ece31f
Tyler Ohlsen <ohltyler@amazon.com>
2024-01-25 12:25:30 -0800

    Refactor runner; add more test cases
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

--- src/tests/anomaly-detection/search_anomaly_detectors.test.ts
+++ src/tests/anomaly-detection/search_anomaly_detectors.test.ts
@@ -3,28 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// TODO fix types
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-
-import fs from 'fs';
 import path from 'path';
 import { ApiProviderFactory } from '../../providers/factory';
-import { QARunner } from '../../runners/qa/qa_runner';
-import { OpenSearchTestIndices } from '../../utils/indices';
+import { AnomalyDetectionRunner } from '../../runners/anomaly_detection';
 
 const provider = ApiProviderFactory.create();
-const runner = new (class AnomalyDetectionRunner extends QARunner {
-  protected async beforeAll(clusterStateId: string): Promise<void> {
-    if (clusterStateId !== 'anomaly-detection') {
-      throw new Error('unexpected cluster state id');
-    }
-    await OpenSearchTestIndices.delete('anomaly-detection');
-    await OpenSearchTestIndices.create('anomaly-detection');
-  }
-})(provider);
+const runner = new AnomalyDetectionRunner(provider);
 
 const specDirectory = path.join(__dirname, 'specs');
 const specFiles = [path.join(specDirectory, 'search_anomaly_detectors_tests.jsonl')];
